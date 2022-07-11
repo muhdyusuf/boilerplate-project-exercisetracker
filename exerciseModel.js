b194f6a28a7c47dfc4ac3e70aa329a02b1973ced
muhdyusuf <92169759+muhdyusuf@users.noreply.github.com>
2022-07-12 00:22:00 +0800

    update bug

--- exerciseModel.js
+++ exerciseModel.js
@@ -11,7 +11,7 @@ const exerciseSchema=new mongoose.Schema({
         required:true,
       },
       date:{
-          type:Date,
+          type:String,
           default:Date.now
       }  
     
